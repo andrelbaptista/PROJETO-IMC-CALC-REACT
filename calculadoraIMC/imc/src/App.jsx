b60b4ca91e5a9341ce33d738e6be40d1f2d20082
Andre Baptista <122994174+andrelbaptista@users.noreply.github.com>
2024-12-28 15:47:11 -0300

    IMC calculado

--- calculadoraIMC/imc/src/App.jsx
+++ calculadoraIMC/imc/src/App.jsx
@@ -1,15 +1,24 @@
-
-import './App.css'
-import ImcCalc from './components/ImcCalc'
+import { useState } from "react";
+import { data } from "./data/data";
+import "./App.css";
+import ImcCalc from "./components/ImcCalc";
+import ImcTable from "./components/ImcTable";
 
 function App() {
- 
+  const calcImc = (e, height, weight) => {
+    e.preventDefault();
+    if (!weight || !height) return;
+    const weightFloat = +weight.replace(",", ".");
+    const heightFloat = +height.replace(",", ".");
+    const imcResult = (weightFloat / (heightFloat* heightFloat)).toFixed(1);
+    setImc(imcResult);
 
-  return (
-   <div className="container">
-    <ImcCalc/>
-   </div>
-  )
+  }
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
+  return <div className="container">{!imc ? <ImcCalc  calcImc={calcImc}/> : <ImcTable />}</div>;
 }
 
-export default App
+export default App;
--- calculadoraIMC/imc/src/components/ImcCalc.jsx
+++ calculadoraIMC/imc/src/components/ImcCalc.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
 import "./ImcCalc.css";
+import Button from "./Button.jsx";
 
-const ImcCalc = () => {
+const ImcCalc = ({calcImc}) => {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+
+  const clearForm = (e) => {
+    e.preventDefault();
+    setHeight("");
+    setWeight("");
+  };
+
+  const validDigits = (text) => {
+    return text.replace(/[^0-9,]/g, "");
+  };
+
+  const handleHeightChange = (e) => {
+    const updateValue = validDigits(e.target.value);
+    setHeight(updateValue);
+  };
+
+  const handleWeightChange = (e) => {
+    const updateValue = validDigits(e.target.value);
+    setWeight(updateValue);
+  };
+
   return (
     <div id="calc-container">
       <h2>Calculadora de IMC</h2>
@@ -13,6 +38,8 @@ const ImcCalc = () => {
               name="height"
               id="height"
               placeholder="Exemplo: 1,75 m"
+              onChange={(e) => handleHeightChange(e)}
+              value={height}
             />
           </div>
           <div className="form-control">
@@ -22,12 +49,14 @@ const ImcCalc = () => {
               name="weight"
               id="weight"
               placeholder="Exemplo: 70,5 kg"
+              onChange={(e) => handleWeightChange(e)}
+              value={weight}
             />
           </div>
         </div>
         <div className="action-control">
-          <button>Calcular</button>
-          <button>Limpar</button>
+          <Button id="calc-btn" text="Calcular" action={(e) => calcImc(e,height, weight)} />
+          <Button id="clear-btn" text="Limpar" action={clearForm} />
         </div>
       </form>
     </div>
